0e51d2b25b286e40fe347e69e3e70a951d21949f
MuhammadRidwan54 <productionridwan54@gmail.com>
2024-07-21 23:35:12 +0700

    update data galery

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -29,6 +29,82 @@ const batik = [
         gambar: "../batik/batik-nusa-tenggara.png",
         deskripsi:"Motif batik lumbu adalah motif dengan budaya menyimpan padi di dalam lumbu. Rumah yang di gambarkan adalah lumbu padi dengan orang-orangan di sampingnya." 
     },
+    {
+        nama: "Batik Mega Mendung",
+        gambar: "../batik/batik-mega-mendung.jpg",
+        deskripsi:"Batik Mega Mendung adalah motif batik yang berasal dari Cirebon, Jawa Barat. Batik Mega Mendung memiliki motif seperti gumpalan awan di langit. Motif awan ini dihiasi dengan gradasi warna yang menarik."
+    },
+    {
+        nama: "Batik Ceplok",
+        gambar: "../batik/batik-ceplok.jpg",
+        deskripsi:"Batik Ceplok berasal dari daerah Bantul, Daerah Istimewa Yogyakarta. Batik Ceplok memiliki motif seperti buah aren yang dibelah menjadi empat dan disusun pada setiap sudutnya."
+    },
+    {
+        nama: "Batik Kawung",
+        gambar: "../batik/batik-kawung.jpg",
+        deskripsi:"Batik Kawung merupakan batik yang berasal dari Daerah Istimewa Yogyakarta. Batik Kawung juga termasuk salah satu batik tertua di Indonesia, bahkan hingga ratusan tahun yang lalu." 
+    },
+    {
+        nama: "Batik Parang",
+        gambar: "../batik/batik-parang.jpg",
+        deskripsi:"batik Parang yang berasal dari Kota Solo. Batik ini memiliki motif seperti gulungan ombak yang disusun secara miring." 
+    },
+    {
+        nama: "Batik Tujuh Rupa",
+        gambar: "../batik/batik-tujuh-rupa.jpeg",
+        deskripsi:"Salah satu batik asal Pekalongan yang terkenal yaitu batik Tujuh Rupa. Batik Tujuh Rupa memiliki motif hewan dan tumbuhan dengan suasana alam yang beragam. Filosofi motif batik ini adalah kebudayaan leluhur Pekalongan yang zaman dahulu termasuk wilayah transit perdagangan." 
+    },
+    {
+        nama: "Batik Tambal",
+        gambar: "../batik/batik-tambal.jpeg",
+        deskripsi:"Batik asal Yogyakarta lainnya adalah batik Tambal. Sesuai namanya, batik Tambal memiliki motif dan filosofi untuk menambal sesuatu yang rusak. Pada awal kemunculannya, batik ini dipakai untuk menyelimuti orang yang sedang sakit karena dipercaya bisa menyembuhkan dengan cepat." 
+    },
+    {
+        nama: "Batik Cuwiri",
+        gambar: "../batik/batik-cuwiri.jpeg",
+        deskripsi:"Batik Cuwiri adalah motif batik yang berasal dari Solo dan Yogyakarta. Batik Cuwiri menggunakan motif meru dan garuda yang gagah dengan warna cerah seperti emas, biru, dan putih. Batik Cuwiri dibuat menggunakan zat pewarna soga alam yang mengeluarkan warna dengan ciri khasnya." 
+    },
+    {
+        nama: "Batik Sekar Jagad",
+        gambar: "../batik/batik-sekar-jagad.jpeg",
+        deskripsi:"Batik Sekar Jagad yang berasal dari Solo dan Yogyakarta. Batik Sekar Jagad memiliki motif dengan warna dan bentuk yang unik.Motif batik Sekar Jagad menunjukkan keberagaman Indonesia karena seakan menggabungkan berbagai motif batik dengan warna yang berbeda-beda. Mulai dari ungu, oranye, merah, cokelat, hingga biru." 
+    },
+    {
+        nama: "Batik Sido Asih",
+        gambar: "../batik/batik-sido-asih.jpg",
+        deskripsi:"Batik ini masuk ke dalam daftar batik yang digunakan dalam pernikahan adat Jawa. Nama batik ini sendiri berasal dari Bahasa Jawa “sido” yang berarti jadi dan terus menerus serta “asih” yang berarti kasih sayang, sehingga arti keseluruhan dari batik ini adalah kasih sayang yang terus menerus. Gambar batik yang indah ini mewakilkan harapan agar pernikahan yang terjadi akan penuh dengan cinta dan kasih sayang." 
+    },
+    {
+        nama: "Batik Gentongan",
+        gambar: "../batik/batik-gentongan.jpg",
+        deskripsi:"Motif batik yang khas dari daerah ini adalah sentuhan flora dan fauna, termasuk dalam batik gentongan yang juga berasal dari Madura. Namun, gambar batik gentongan lebih condong ke motif flora dan abstrak." 
+    },
+    {
+        nama: "Batik Sido Luhur",
+        gambar: "../batik/batik-sido-luhur.jpg",
+        deskripsi:"Motif ini sangat erat dengan budaya Jawa, di mana dalam hidup harus mencari keluhuran baik secara materi maupun non materi. Keluhuran materi bisa berupa pangkat, derajat, dan jabatan. Sedangkan keluhuran non materi merupakan hubungan dengan Sang Pencipta." 
+    },
+    {
+        nama: "Batik Priyangan",
+        gambar: "../batik/batik-priyangan.jpg",
+        deskripsi:"Batik Priyangan sangat menggambarkan masyarakatnya. Motifnya dibuat rapat dan sangat rapi. Memberi kesan bahwa masyarakat Priyangan secara umum terbuka dan pluralis. Motifnya juga bermakna kesederhanaan. Warnanya ada yang cerah dan ada juga yang kalem. Untuk warna gelapnya, ada hitam dan hijau." 
+    },
+    {
+        nama: "Batik Garutan",
+        gambar: "../batik/batik-garutan.jpg",
+        deskripsi:"Motif garutan biasanya didominasi warna krem yang lalu dipadukan dengan warna-warni cerah yang membuatnya berbeda dari batik lainnya. Misalnya warna pink, ungu muda, dan lain-lain. Tak heran kalau motif ini menarik hati anak muda. Biasanya motif Garutan berbegntuk geometri. Ada juga yang bermotif flora dan fauna." 
+    },
+    {
+        nama: "Batik Tubo",
+        gambar: "../batik/batik-tubo.jpg",
+        deskripsi:"Banyak yang belum familier dengan batik Tobo asal Ternate, Maluku Utara. Padahal motifnya cantik, mencerminkan kehidupan warga Ternate sehari-hari. Karena itu ada motif pala, cengkeh, pisang bebe, ikan, dan burung Halmahera yang ada di sekitar mereka. Filosofinya adalah kedekatan warga Ternate dengan kekayaan alam yang diberikan Tuhan." 
+    },
+    {
+        nama: "Batik Ulamsari Mas",
+        gambar: "../batik/batik-ulamsari-mas.jpg",
+        deskripsi:"Motif utamanya adalah ikan dan udang yang berdampingan. Makna di baliknya adalah mata pencaharian kebanyakan masyarakat Bali yang bekerja sebagai nelayan." 
+    },
+
     
 ];
 
